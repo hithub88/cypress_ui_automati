494872ef889cbaa6878ac291e19ab2b24857df0c
unknown <agpresas118@gmail.com>
2024-05-04 16:28:09 +0800

    test

---
+++ cypress/e2e/test.cy.js
@@ -0,0 +1,21 @@
+describe('Katalon Application', ()=> {
+
+    it("Home Page", ()=> {
+
+        cy.visit('https://katalon-demo-cura.herokuapp.com/');
+        cy.get('#btn-make-appointment').click();
+        cy.get('#txt-username').type('John Doe');
+        cy.get('#txt-password').type('ThisIsNotAPassword');
+        cy.get('#btn-login').click();
+        cy.get('#chk_hospotal_readmission').click();
+        cy.get('#txt_visit_date').type('05/04/2024');
+        cy.get('#btn-book-appointment').click();
+
+    }
+)
+
+
+
+
+
+})
